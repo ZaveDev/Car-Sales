ba7478127cfcc0f235cb49872afe470fa85e6de3
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-08-12 11:44:19 -0400

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import {connect} from 'react-redux'
+import {addFeature} from '../actions'
+
+
+
+
 const AdditionalFeature = props => {
+  console.log(props);
+  const featureAdded = e => {
+    e.preventDefault()
+    props.addFeature(props.feature)
+  }
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={featureAdded}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+
+export default connect(null, {addFeature})(AdditionalFeature);
